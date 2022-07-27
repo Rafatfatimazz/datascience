a5f219e1a8386e8606eb61e7d5c946540626a903
rafat <ak6583655@gmail.com>
2022-07-27 10:13:34 +0530

    plus symbol

---
+++ plus.py
@@ -0,0 +1,10 @@
+from turtle import*
+for i in range(4):
+    lt(90)
+    fd(100)
+    right(90)
+    fd(100)
+    rt(90)
+    fd(100)    
+
+mainloop()
--- symbol.py
+++ symbol.py
@@ -1,22 +1,16 @@
 from turtle import*
 speed("fastest")
 pencolor("green")
-bgcolor("yellow")
-pensize(10)
-side=12
-size=100
-fillcolor("red")
+bgcolor("blue")
+color("yellow","blue")
+pensize(1)
 begin_fill()
-fd(size)
-lt(90)
-fd(size)
-rt(90)
-fd(size)
-fd(size)
-lt(90)
-fd(size)
-rt(90)
-fd(size)
+while True:
+    fd(200)
+    lt(170)
+    if abs(pos())<1:
+      break
+
 
 end_fill()
 
